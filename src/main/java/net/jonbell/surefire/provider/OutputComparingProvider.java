993eb657be578c6479baa05459df1ce9ca02a6fd
Jonathan Bell <jbell@cs.columbia.edu>
2016-11-04 14:29:54 -0400

    fix broken lines in runner

--- src/main/java/net/jonbell/surefire/provider/OutputComparingProvider.java
+++ src/main/java/net/jonbell/surefire/provider/OutputComparingProvider.java
@@ -3,6 +3,7 @@ package net.jonbell.surefire.provider;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
+import org.apache.maven.surefire.junitcore.JUnitCoreProvider;
 import org.apache.maven.surefire.providerapi.AbstractProvider;
 import org.apache.maven.surefire.providerapi.ProviderParameters;
 import org.apache.maven.surefire.report.ReporterException;
@@ -11,28 +12,35 @@ import org.apache.maven.surefire.testset.TestSetFailedException;
 
 public class OutputComparingProvider extends AbstractProvider {
 	private AbstractProvider[] providers;
-	
+	private JUnitCoreProvider baseProvider;
 	public OutputComparingProvider(ProviderParameters booterParameters)
 	{
+		baseProvider = new JUnitCoreProvider(booterParameters);
 		String cps = booterParameters.getProviderProperties().get("cp");
 		String[] cp = cps.split(",");
 		providers = new AbstractProvider[cp.length];
 		for(int i = 0; i< cp.length; i++)
 		{
 			cp[i] = cp[i].trim();
-			providers[i] = new 
+//			providers[i] = new 
 		}
+		System.out.println("OCP");
 		System.out.println(Arrays.toString(cp));
 	}
 
 	public Iterable<Class<?>> getSuites() {
-		System.err.println("Get suites");
-		return null;
+		Iterable<Class<?>> ret = baseProvider.getSuites();
+		System.out.println("Suites: " + ret);
+		return ret;
+//		return null;
 	}
 
 	public RunResult invoke(Object forkTestSet)
 			throws TestSetFailedException, ReporterException, InvocationTargetException {
+		System.out.println("RunResult invoke");
+		System.out.println(getSuites());
 		System.err.println("Invoke");
+		System.out.println(forkTestSet);
 		return null;
 	}
 }
